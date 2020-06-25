6ec1dbe342dec0441ecbd372d51720f6a2fa9e05
Shashank <ss9621963652@gmail.com>
2020-06-26 00:54:09 +0530

    Some files added

---
+++ main.js
@@ -0,0 +1,49 @@
+// document.addEventListener("click",darkMode());
+
+
+function darkMode(){
+//
+// var clicked=document.getElementById("customSwitch1");
+// console.log(clicked);
+
+var element = document.body;
+element.classList.toggle("dark-mode");
+
+var element = document.getElementById("cardbody");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody1");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody2");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody3");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody4");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody5");
+element.classList.toggle("dark-mode1");
+
+
+var element = document.getElementById("cardbody6");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody7");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody8");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody9");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody10");
+element.classList.toggle("dark-mode1");
+
+var element = document.getElementById("cardbody11");
+element.classList.toggle("dark-mode1");
+
+}
